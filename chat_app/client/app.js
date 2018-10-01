19a7137c9292cc29f262862d9cca8bb31e65752c
Minamiciccc <june28munazo@gmail.com>
2018-10-01 16:55:35 -0700

    stored messages in an array

--- chat_app/client/app.js
+++ chat_app/client/app.js
@@ -1,7 +1,9 @@
 var socket = io();
+
+socket.emit('login');
+
 $('form').on('submit',function() {
     var text = $('#initials').val() + " says: " + $('#message').val();
-    
     socket.emit('message', text);
     $('message').val('');
     return false;
@@ -10,3 +12,9 @@ $('form').on('submit',function() {
 socket.on('message', function(msg) {
     $('<li>').text(msg).appendTo('#history');
 });
+
+socket.on('login', function(messages) {
+    messages.forEach(msg => {
+        $('<li>').text(msg).appendTo('#history');
+    });
+})
--- chat_app/server.js
+++ chat_app/server.js
@@ -7,10 +7,15 @@ var server = http.Server(app);
 app.use(express.static('client'));
 
 var io = require('socket.io')(server);
+var messages = [];
 
 io.on('connection', function (socket) {
     socket.on('message', function(msg) {
+        messages.push(msg)
         io.emit('message', msg);
+    });
+    socket.on('login', () => {
+        io.emit('login', messages);
     });
 });
 
