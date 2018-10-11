0066d4d19712f745c9bd0034f92945eb3ed2c2fe
Minami Munakata <june28munazo@gmail.com>
2018-10-11 11:31:49 -0700

    add day9

---
+++ challenge21day/challenge9.js
@@ -0,0 +1,28 @@
+const GRID = [
+    ["", "", "", "^", "", "", "", "", "", ""],
+    ["", "", "v", "", "~", "", "", "", "", ""],
+    ["", "v", "", "", "^", "^", "", "", "", ""],
+    ["", "", "", "", "^", "^", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "v", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "^", "~", "~", "", "", "", "^", "", ""],
+    ["", "^", "", "~", "~", "", "", "", "", ""],
+    ["", "^", "", "", "~", "~", "", "", "", ""],
+  ];
+
+let alph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"];
+
+function convertColumn(coordinates) {
+    return alph.indexOf(coordinates[0]);
+}
+
+function lightCell(coordinates) {
+    return GRID[coordinates[1] - 1][convertColumn(coordinates)];
+}
+
+function isShip(coordinates) {
+    return lightCell(coordinates) === "v";
+}
