ac9f0e91a3979503e917fbf2a37ce6d35bb74bde
Minami Munakata <june28munazo@gmail.com>
2018-10-17 10:40:50 -0700

    day15

---
+++ challenge21day/challenge15.js
@@ -0,0 +1,45 @@
+const GRID = [
+    ["", "", "", "^", "", "", "", "", "", ""],
+    ["", "", "v", "", "~", "", "", "", "", ""],
+    ["", "v", "", "", "^", "^", "", "", "", ""],
+    ["", "", "", "", "^", "^", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "v", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "", "", "", "", "", "", "", "", ""],
+    ["", "^", "~", "~", "", "", "", "^", "", ""],
+    ["", "^", "", "~", "~", "", "", "", "", ""],
+    ["", "^", "", "", "~", "~", "", "", "", ""],
+  ];
+
+let alph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
+            "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"];
+
+function countRows() {
+    return GRID.length;
+}
+
+function convertColumn(coordinates) {
+    return alph.indexOf(coordinates[0]);
+}
+
+function lightCell(coordinates) {
+    return GRID[coordinates.substring(1)  - 1][convertColumn(coordinates)];
+}
+
+function isRock(coordinates) {
+    return lightCell(coordinates) === "^";
+}
+
+function firstRock() {
+    let index = 0;
+    for (let row = 1; row <= countRows(); row++) {
+        for (const col in alph) {
+            const coordinates = alph[col] + row;
+            if (isRock(coordinates)) {
+                return coordinates;
+            }
+        }
+    }
+    return null;
+}
